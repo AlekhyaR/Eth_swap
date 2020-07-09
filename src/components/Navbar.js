f6d55fcf9a94a6c25e21ab1f12586e2a3d31decf
alekhya <alekhya.rudra@cybrilla.com>
2020-07-09 14:11:51 +0530

    adds styling to account number displaying in navbar 1. adds icon beside account in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Identicon from 'identicon.js'
 
 class Navbar extends Component {
   render() {
@@ -12,7 +13,23 @@ class Navbar extends Component {
         >
           EthSwap
         </a>
-        <p>{this.props.account}</p>
+        <ul className="navbar-nav px-3">
+          <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+            <small className="text-secondary">
+              <small id="account">{this.props.account}</small>
+            </small>
+            { this.props.account
+              ? <img
+                className="ml-2"
+                width='30'
+                height='30'
+                src={`data:image/png;base64,${new Identicon(this.props.account, 30).toString()}`}
+                alt=""
+              />
+              : <span></span>
+            }
+          </li>
+        </ul>
       </nav>
     );
   }
